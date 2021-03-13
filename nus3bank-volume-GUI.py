55e058c88d2f9893306301723d2dab38aed8c458
Pacil142857 <7coolaustin7@gmail.com>
2021-03-13 10:24:58 -0500

    Added function to get the old volume of a nus3bank.

--- nus3bank-volume-GUI.py
+++ nus3bank-volume-GUI.py
@@ -29,74 +29,83 @@ def hex_to_float(hx):
 '''
 regularInput = False
 if len(sys.argv) > 4 or len(sys.argv) == 2:
-    #More than 3 command-line arguments or 1 command-line argument
+    # More than 3 command-line arguments or 1 command-line argument
     raise ArgumentError
 elif len(sys.argv) == 4:
-    #3 command-line arguments
+    # 3 command-line arguments
     path = sys.argv[1]
     entry = int(sys.argv[2])
     volumeFloat = float(sys.argv[3])
 elif len(sys.argv) == 3:
-    #2 command-line arguments
+    # 2 command-line arguments
     path = sys.argv[1]
     entry = 0
     volumeFloat = float(sys.argv[2])
 else:
     path = input("NUS3BANK path: ")
-    path = path.replace('"', '') #Remove any quotes in input
+    path = path.replace('"', '') # Remove any quotes in input
     entry = input("Entry (leave blank if music): ")
     if entry == "":
         entry = 0
     else:
         entry = int(entry)
     regularInput = True
-
-key = b'\xe8\x22\x00\x00'
-try:
-    if path[-9:] != ".nus3bank":
-        raise ExtensionError
-    with open(path, "rb+") as f:
-        occurance = 0
-        content = bytearray(f.read())
-
-        backupName = path[:-9] + ".nus3bank.bak"
-        with open(backupName, "wb") as backup:
-            backup.write(content)
-
-        for i in range(len(content)):
-            if key == content[i:i+4]:
-                occurance += 1
-            if occurance == entry + 2:
-                break
-        else:
-            raise EntryError
-
-        oldVolume = hex_to_float(int.from_bytes(content[i+4:i+8], byteorder='little'))
-        print("Old volume: " + str(oldVolume))
-        #print("Changing volume of entry " + str(entry) + " from " + str(oldVolume) + " to " + str(volumeFloat) + "...")
-        if regularInput:
-            volumeFloat = float(input("New volume (decimal and negative values are valid): "))
-        volume = float_to_hex(volumeFloat)
-        volume = volume.to_bytes(4, 'big')
-
-        #Write to the beginning and erase the end
-        content[i+4:i+8] = volume
-        f.seek(0)
-        f.write(content)
-        f.truncate(len(content))
-
-except ArgumentError:
-    print("Incorrect number of arguments.")
-except EntryError:
-    print("Entry number not found.")
-except ExtensionError:
-    print("File did not end with .nus3bank.")
-except:
-    print("An unknown error has occurred.")
-finally:
-    f.close()
-    backup.close()
 '''
+
+def getVolume(path, entry):
+    key = b'\xe8\x22\x00\x00'
+    try:
+        if path[-9:] != ".nus3bank":
+            raise ExtensionError
+        with open(path, "rb+") as f:
+            occurance = 0
+            content = bytearray(f.read())
+
+            ''' No need to make a backup if we're just getting the volume
+            backupName = path[:-9] + ".nus3bank.bak"
+            with open(backupName, "wb") as backup:
+                backup.write(content)
+            '''
+
+            for i in range(len(content)):
+                if key == content[i:i+4]:
+                    occurance += 1
+                if occurance == entry + 2:
+                    break
+            else:
+                raise EntryError
+
+            oldVolume = hex_to_float(int.from_bytes(content[i+4:i+8], byteorder='little'))
+            return oldVolume
+
+            ''' The rest of this writes to the file, so it's commented out for now
+            print("Old volume: " + str(oldVolume))
+            # print("Changing volume of entry " + str(entry) + " from " + str(oldVolume) + " to " + str(volumeFloat) + "...")
+            
+            volumeFloat = float(input("New volume (decimal and negative values are valid): "))
+            volume = float_to_hex(volumeFloat)
+            volume = volume.to_bytes(4, 'big')
+
+            # Write to the beginning and erase the end
+            content[i+4:i+8] = volume
+            f.seek(0)
+            f.write(content)
+            f.truncate(len(content))
+            '''
+
+    except ArgumentError:
+        print("Incorrect number of arguments.")
+    except EntryError:
+        print("Entry number not found.")
+    except ExtensionError:
+        print("File did not end with .nus3bank.")
+    except:
+        print("An unknown error has occurred.")
+    finally:
+        f.close()
+        # Commented out since we didn't make a backup
+        # backup.close()
+
 
 # A list of file extensions for nus3bank files
 fileExtensions = (('NUS3BANK files', '*.nus3bank'), ('Backup NUS3BANK files', '*.nus3bank.bak'))
@@ -112,8 +121,6 @@ layout = [ [sg.Text('Entry (put 0 if music):')],
            [sg.Button('Get old volume', key='Submit')]]
 
 window = sg.Window('Nus3bank Volume GUI', layout)
-
-print(window['Entry'].BackgroundColor)
 
 # Handle events
 while True:
@@ -131,6 +138,7 @@ while True:
     if event == 'Submit':
         print(f'Entry: {values["Entry"]}')
         print(f'File: {values["nus3bankFile"]}')
-        # TODO: Get the volume when the user presses submit
+        origVol = getVolume(values['nus3bankFile'], int(values['Entry']))
+        print(origVol)
 
 window.close()
